7eb4245109e1fd4f7e0a607e89b3ddb87d4004b4
Alex <alexhandel96@googlemail.com>
2019-09-05 13:35:56 -0300

    temp

--- Assets/storyController.cs
+++ Assets/storyController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class storyController : MonoBehaviour
 {
     public GameObject text;
-    public float time, waitTime;
+    public float time, waitTime, fadeoutTime;
     public bool isDone=false;
+    public Color color;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,10 @@ public class storyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.anyKey)
+        {
+            changeScene();
+        }
         if (text.GetComponent<textTyper>().finished&&!isDone)
         {
             time = Time.time;
@@ -29,6 +35,17 @@ public class storyController : MonoBehaviour
     }
     public void changeScene()
     {
+        StartCoroutine(FadeImage());
+    }
+    IEnumerator FadeImage()
+    {
+        // loop over 1 second backwards
+        for (float i = fadeoutTime; i >= 0; i -= Time.deltaTime)
+        {
+            // set color with i as alpha
+            text.GetComponent<TextMeshProUGUI>().color = new Color(color.r, color.g, color.b, i/fadeoutTime);
+            yield return null;
+        }
         SceneManager.LoadScene(1);
     }
 }
