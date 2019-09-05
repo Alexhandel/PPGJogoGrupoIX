a5b74e3a2a548f1c40682c2bad1d48cd9e44420c
Alex <alexhandel96@googlemail.com>
2019-09-05 14:02:39 -0300

    Final de verdade

--- Assets/Scripts/menuController.cs
+++ Assets/Scripts/menuController.cs
@@ -50,8 +50,8 @@ public class menuController : MonoBehaviour
     {
         if (diffi=="easy")
         {
-            difficultyKeeper.GetComponent<difficultyKeeeper>().enemyHealth = 6;
-            difficultyKeeper.GetComponent<difficultyKeeeper>().playerHealth = 15;
+            difficultyKeeper.GetComponent<difficultyKeeeper>().enemyHealth = 9;
+            difficultyKeeper.GetComponent<difficultyKeeeper>().playerHealth = 8;
             difficultyKeeper.GetComponent<difficultyKeeeper>().bombNumber = 3;
             difficultyKeeper.GetComponent<difficultyKeeeper>().enemyAttackSpeed = 1.2f;
             difficultyKeeper.GetComponent<difficultyKeeeper>().enemyShieldTimer = 2f;
@@ -59,7 +59,7 @@ public class menuController : MonoBehaviour
         else if (diffi=="medium")
         {
             difficultyKeeper.GetComponent<difficultyKeeeper>().enemyHealth = 9;
-            difficultyKeeper.GetComponent<difficultyKeeeper>().playerHealth = 10;
+            difficultyKeeper.GetComponent<difficultyKeeeper>().playerHealth = 8;
             difficultyKeeper.GetComponent<difficultyKeeeper>().bombNumber = 2;
             difficultyKeeper.GetComponent<difficultyKeeeper>().enemyAttackSpeed = 1f;
             difficultyKeeper.GetComponent<difficultyKeeeper>().enemyShieldTimer = 1.5f;
@@ -68,7 +68,7 @@ public class menuController : MonoBehaviour
         else if (diffi == "hard")
         {
             difficultyKeeper.GetComponent<difficultyKeeeper>().enemyHealth = 12;
-            difficultyKeeper.GetComponent<difficultyKeeeper>().playerHealth = 8;
+            difficultyKeeper.GetComponent<difficultyKeeeper>().playerHealth = 10;
             difficultyKeeper.GetComponent<difficultyKeeeper>().bombNumber = 1;
             difficultyKeeper.GetComponent<difficultyKeeeper>().enemyAttackSpeed = 0.8f;
             difficultyKeeper.GetComponent<difficultyKeeeper>().enemyShieldTimer = 1f;
